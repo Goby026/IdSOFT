4e632c34bfa0627d7d7fa7f54d63241a0643ee48
Grover <george.rendich@gmail.com>
2015-10-16 00:02:13 -0500

    Registro de usuarios: Archivo php de registro de usuarios.

---
+++ reg_usuario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro de usuarios</title>
+</head>
+<body>
+	<header>
+		Cabecera
+		<h1>IDEAS EN SOFTWARE</h1>
+	</header>
+	<nav>
+		Barra de navegación
+	</nav>
+	<section>
+		Contenido principal
+	</section>
+	<footer>IdSoft.hyo@hotmail.com</footer>
+</body>
+</html>
